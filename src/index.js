b8d722119f78542d3973b989a85d615e8f0cebb2
thejohnpage <john@thejohnpage.com>
2020-01-22 09:30:55 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
+const TransactionMiner = require('./app/transaction-miner');
+
+const app = express();
+const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
+const pubsub = new PubSub({blockchain, transactionPool});
+const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+
+app.use(bodyParser.json());
+
+app.get('/api/blocks',(req, res) => {
+    res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+    const {data} = req.body;
+    blockchain.addBlock({data});
+    pubsub.broadcastChain();
+    res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+    const { amount, recipient } = req.body;
+    let transaction = transactionPool.existingTransaction({ inputAddress: wallet.publicKey});
+    try {
+        if(transaction) {
+            transaction.update({ senderWallet: wallet, recipient, amount });
+        } else {
+            transaction = wallet.createTransaction({
+                recipient,
+                amount,
+                chain: blockchain.chain
+            });
+        }
+        transactionPool.setTransaction(transaction);
+        pubsub.broadcastTransaction(transaction);
+        res.json({type: 'success', transaction});
+    } catch (e) {
+        res.status(400).json({type: 'error', message: e.message});
+    }
+});
+
+app.get('/api/transaction-pool-map', (req, res) => {
+    res.json(transactionPool.transactionMap);
+});
+
+app.get('/api/mine-transactions', (req, res) => {
+    transactionMiner.mineTransactions();
+    res.redirect('/api/blocks');
+});
+
+const syncWithRootState = () => {
+    request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+        if(!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+            console.log('replace chain on sync with ', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+
+    request({url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map`}, (error, response, body) => {
+        if(!error && response.statusCode === 200) {
+            const rootTransactionPoolMap = JSON.parse(body);
+            console.log('replace transaction pool map on a syncWithRootState: ', rootTransactionPoolMap);
+            transactionPool.setMap(rootTransactionPoolMap);
+        }
+    });
+};
+
+let PEER_PORT;
+if(process.env.GENERATE_PEER_PORT === 'true') {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+app.listen(PORT, () => {
+    console.log(`listening at localhost:${PORT}`);
+    if (PORT !== DEFAULT_PORT) {
+        syncWithRootState();
+    }
+});
